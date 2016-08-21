570c6ea14fe15aa5059ce21d458f499aa0ee63b8
sthpravin <sthpravin@gmail.com>
2016-08-21 22:22:19 +0545

    Model and cpntroller for comments added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+	before_action :is_owner?, only: [:edit, :update, :destroy]
 	before_action :authenticate_user!, only: [:new, :create]
 	def index
 		@posts = Post.all.order('created_at DESC')
@@ -7,6 +7,10 @@ class PostsController < ApplicationController
 	def new
 		@post = Post.new
 	end
+	def edit
+  		@post = Post.find(params[:id])
+	end
+
 	def create
   		@post = current_user.posts.create(post_params)
   		if @post.valid?
@@ -15,10 +19,33 @@ class PostsController < ApplicationController
     		render :new, status: :unprocessable_entity
   		end
 	end
+	def update
+  		@post = Post.find(params[:id])
+  		@post.update(post_params)
+  		if @post.valid?
+    		redirect_to root_path
+  		else
+    		render :edit, status: :unprocessable_entity
+  		end
+	end
+	def destroy
+  		@post = Post.find(params[:id])
+  		@post.delete
+  		redirect_to root_path
+	end
+	def show
+  		@post = Post.find(params[:id])
+	end
+
 
 	private
 	
 	def post_params
   		params.require(:post).permit(:user_id, :photo, :description)
+	end
+	def is_owner?
+  		if Post.find(params[:id]).user != current_user
+    		redirect_to root_path
+  		end
 	end
 end
